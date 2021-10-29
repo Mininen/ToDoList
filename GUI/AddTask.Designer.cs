eb9a0f2a62f3338d4712f5d92a99d78d71803c2a
Miriam <88656262+Mininen@users.noreply.github.com>
2021-10-29 15:17:51 +0200

    Add files via upload

---
+++ GUI/AddTask.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace GUI
+{
+    partial class AddTask
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameTB = new System.Windows.Forms.TextBox();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DescriptionTB = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.addTaskBtn = new System.Windows.Forms.Button();
+            this.CancelBtn = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // nameTB
+            // 
+            this.nameTB.Location = new System.Drawing.Point(139, 44);
+            this.nameTB.Name = "nameTB";
+            this.nameTB.Size = new System.Drawing.Size(214, 26);
+            this.nameTB.TabIndex = 0;
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Location = new System.Drawing.Point(139, 311);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(214, 26);
+            this.dateTimePicker.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(66, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 22);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name:";
+            // 
+            // DescriptionTB
+            // 
+            this.DescriptionTB.Location = new System.Drawing.Point(139, 93);
+            this.DescriptionTB.Name = "DescriptionTB";
+            this.DescriptionTB.Size = new System.Drawing.Size(214, 196);
+            this.DescriptionTB.TabIndex = 4;
+            this.DescriptionTB.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 315);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 22);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Due date:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 22);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Description:";
+            // 
+            // addTaskBtn
+            // 
+            this.addTaskBtn.Location = new System.Drawing.Point(139, 357);
+            this.addTaskBtn.Name = "addTaskBtn";
+            this.addTaskBtn.Size = new System.Drawing.Size(104, 38);
+            this.addTaskBtn.TabIndex = 7;
+            this.addTaskBtn.Text = "Add task";
+            this.addTaskBtn.UseVisualStyleBackColor = true;
+            this.addTaskBtn.Click += new System.EventHandler(this.addTaskBtn_Click);
+            // 
+            // CancelBtn
+            // 
+            this.CancelBtn.Location = new System.Drawing.Point(249, 357);
+            this.CancelBtn.Name = "CancelBtn";
+            this.CancelBtn.Size = new System.Drawing.Size(104, 38);
+            this.CancelBtn.TabIndex = 8;
+            this.CancelBtn.Text = "Cancel";
+            this.CancelBtn.UseVisualStyleBackColor = true;
+            this.CancelBtn.Click += new System.EventHandler(this.CancelBtn_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(172, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(142, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Add new task";
+            // 
+            // AddTask
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 455);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.CancelBtn);
+            this.Controls.Add(this.addTaskBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.DescriptionTB);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.nameTB);
+            this.Name = "AddTask";
+            this.Text = "AddTask";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nameTB;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox DescriptionTB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button addTaskBtn;
+        private System.Windows.Forms.Button CancelBtn;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ GUI/AddTask.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BusinessLayer;
+using Entities;
+
+namespace GUI
+{
+    public partial class AddTask : Form
+    {
+        BusinessManager bm { get; set; }
+        public AddTask(BusinessManager businessManager)
+        {
+            bm = businessManager;
+            InitializeComponent();
+        }
+
+        private void addTaskBtn_Click(object sender, EventArgs e)
+        {
+            string name = nameTB.Text;
+            string description = DescriptionTB.Text;
+            DateTime dueDate = dateTimePicker.Value;
+            DateTime now = new DateTime();
+            var date = now.Date;
+
+            if (dueDate >= date && name != "") 
+            {
+                bm.addTask(name, description, dueDate);
+                MessageBox.Show("Task Added!");
+
+                this.Hide();
+                Form1 form1 = new Form1(bm);
+                form1.ShowDialog();
+                this.Close();
+            }
+            else MessageBox.Show("Tasks must have a name and deadline have to be in the future!");
+        }
+
+        private void CancelBtn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("No new task added");
+            this.Hide();
+            Form1 form1 = new Form1(bm);
+            form1.ShowDialog();
+            this.Close();
+        }
+    }
+}
