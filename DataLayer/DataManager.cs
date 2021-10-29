2339a058c96258dcf26c30b624186f0ed9205960
Miriam <88656262+Mininen@users.noreply.github.com>
2021-10-29 15:19:56 +0200

    Add files via upload

---
+++ DataLayer/DataManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+using System.IO;
+
+namespace DataLayer
+{
+    public class DataManager
+    {
+        public void SaveToFile(List<TaskItem> tasks)
+        {
+            StreamWriter sw = new StreamWriter("ToDo.txt");
+
+            foreach (TaskItem t in tasks)
+            {
+                sw.WriteLine(t.Name + "\t" + t.Description + "\t" + t.Deadline.ToString());
+            }
+
+            sw.Close();
+        }
+
+        public List<TaskItem> ReadFile()
+        {
+            StreamReader sr = new StreamReader("ToDo.txt");
+            string row;
+
+            List<TaskItem> tasks = new List<TaskItem>();
+
+            while ((row = sr.ReadLine()) != null)
+            {
+                string[] splitRow = row.Split('\t');
+                TaskItem t = new TaskItem();
+                t.Name = splitRow[0];
+                t.Description = splitRow[1];
+                t.Deadline = DateTime.Parse(splitRow[2]);
+
+                tasks.Add(t);
+            }
+
+            sr.Close();
+
+            return tasks;
+        }
+    }
+}
