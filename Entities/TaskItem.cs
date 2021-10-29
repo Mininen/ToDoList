15615f0e61b6075286e7fd3baf3838f669fdd54e
Miriam <88656262+Mininen@users.noreply.github.com>
2021-10-29 15:19:00 +0200

    Add files via upload

---
+++ Entities/TaskItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class TaskItem
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Deadline { get; set; }
+
+        public TaskItem(string name, string description, DateTime deadline)
+        {
+            Name = name;
+            Description = description;
+            Deadline = deadline;
+        }
+
+        public TaskItem()
+        {
+
+        }
+    }
+}
